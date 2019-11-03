5d850630de7cbf2a30b9459d8b9d26134a972346
Jason Flower <35689497+JJ-Styles@users.noreply.github.com>
2019-11-03 20:57:39 +0000

    Project Upload

---
+++ Postman challenge/Postman challenge/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim floor As Integer
+        Dim file As String
+        Dim i As Integer
+        Dim FileReader As StreamReader = New StreamReader("Advent1.txt")
+        file = FileReader.ReadToEnd
+        For i = 1 To file.Length
+            If Mid(file, i, 1) = "(" Then
+                floor += 1
+            ElseIf Mid(file, i, 1) = ")" Then
+                floor -= 1
+            End If
+        Next
+        Console.WriteLine("The postman will end up on floor " & floor)
+        Console.ReadKey()
+    End Sub
+
+End Module
